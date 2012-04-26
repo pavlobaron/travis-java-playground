915c22ee3498f6162830b98758410e61983dfcd8
pavlobaron <pb@pbit.org>
2012-04-26 09:30:11 +0200

    initial commit

---
+++ src/org/pbit/ci/travis/java/Main.java
@@ -0,0 +1,14 @@
+package org.pbit.ci.travis.java;
+
+/**
+ * Created: Thu Apr 26 09:02:12 2012
+ *
+ * @author pb
+ * @version 1.0
+ */
+public class Main {
+
+    public static void main(String [] args) {
+	System.out.println("hey, travis");
+    }
+}
