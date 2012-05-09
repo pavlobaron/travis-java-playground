d349cd32bb2c651c733ad2b35b2ece6072d7836c
pavlobaron <pb@pbit.org>
2012-05-09 14:51:04 +0200

    fit for travis

---
+++ test/org/pbit/ci/travis/java/TestTest.java
@@ -0,0 +1,13 @@
+package org.pbit.ci.travis.java;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestTest {
+
+    @Test
+    public void testTest() {
+	assertTrue(1 == 1);
+    }
+}
+
